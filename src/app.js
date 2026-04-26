e11478c46db38bc81633e582b4d8064968b0a013
bigoluwagentle <olayiwolaolabode06@gmail.com>
2026-04-26 14:49:29 -0700

    fixed

--- src/app.js
+++ src/app.js
@@ -19,9 +19,10 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use(cookieParser());
 app.use(requestLogger);
-app.use(csrfProtection);
 
 app.use("/auth", authRoutes);
+
+app.use(csrfProtection);
 app.use("/api/profiles", profileRoutes);
 
 app.use((req, res) => {
